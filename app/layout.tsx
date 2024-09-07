0f2a63e91f2109781855e21eccf7339425c9dc25
Victor Stodell <victor@stodell.se>
2024-09-07 14:17:17 +0200

    Remove unused styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Theme } from '@radix-ui/themes';
-import localFont from "next/font/local";
 import '@radix-ui/themes/styles.css';
-import "./styles/globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Swedball",
@@ -27,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html>
+      <body>
         <ThemeProvider attribute="class">
           <Theme>
             {children}
