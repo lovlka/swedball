f2c576b03925eb91415e23dad8d2469724a4d666
Victor Stodell <victor@stodell.se>
2024-09-09 11:31:19 +0200

    Added dummy api key header

--- app/actions/dummy-api.ts
+++ app/actions/dummy-api.ts
@@ -1,7 +1,21 @@
 'use server';
- 
+
+const ApiUrl: string = 'https://dummyapi.online/api';
+const ApiKey: string | undefined = process.env.DUMMY_API_KEY;
+
+function getRequestInit(): RequestInit {
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  if (ApiKey) {
+    headers['X-Api-Key'] = ApiKey;
+  }
+  return { headers };
+}
+    
 export async function fetchUser(userId: number) {
-  const res = await fetch(`https://dummyapi.online/api/users/${userId}`);
+  const res = await fetch(`${ApiUrl}/users/${userId}`, getRequestInit());
   const data = await res.json();
   return data;
 }
