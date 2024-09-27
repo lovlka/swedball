47bfa2c3201d5ee6b8fffec9557bce6e0c7f7f92
Victor Stodell <victor@stodell.se>
2024-09-27 14:13:03 +0200

    Removed unused code

--- app/components/search-result.tsx
+++ app/components/search-result.tsx
@@ -1,4 +1,4 @@
-import { Card, DataList } from '@radix-ui/themes';
+import { Card } from '@radix-ui/themes';
 import { useAppSelector } from '@/state/hooks';
 
 export default function SearchResult() {
@@ -9,20 +9,6 @@ export default function SearchResult() {
       <pre>
         {JSON.stringify(result, null, 2)}
       </pre>
-      {/*result.hits.map(hit => (
-        <DataList.Root>
-          {Object.entries(hit).map(([key, value]) => (
-            <DataList.Item key={key}>
-              <DataList.Label minWidth="80px" className="capitalize">
-                {key}
-              </DataList.Label>
-              <DataList.Value>
-                {typeof value === 'object' ? Object.values(value).join(', ') : value}
-              </DataList.Value>
-            </DataList.Item>
-          ))}
-        </DataList.Root>
-      ))*/}
     </Card>
   );
 }
